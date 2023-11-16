54d26e56533ecbff5b55ea39c6243f1a0fa9c4e3
sotto97 <sotto.bw97@gmail.com>
2023-11-16 17:50:47 +0900

    TOP画面作成

--- server/test-amazon-trance/routes/web.php
+++ server/test-amazon-trance/routes/web.php
@@ -22,10 +22,15 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// プロフィール編集
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::prefix('coffee')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+});
+
+require __DIR__ . '/auth.php';
