7b6a6bb234700584091dfffd11c9119493e83247
sotto97 <sotto.bw97@gmail.com>
2023-11-16 22:05:30 +0900

    トップ画面を作成

--- server/test-amazon-trance/resources/views/welcome.blade.php
+++ server/test-amazon-trance/resources/views/welcome.blade.php
@@ -15,17 +15,16 @@
 </head>
 
 <body>
-    <div class="bg-white py-6 sm:py-8 lg:py-12">
+    <div class="h-screen flex justify-center items-center bg-white">
         <div class="mx-auto px-4 md:px-8">
             <div class="mx-auto flex flex-col items-center text-center">
-                <p class="font-semibold text-yellow-500 md:text-lg xl:text-xl">Proud to introduce</p>
+                <p class="font-semibold text-yellow-500 md:text-lg xl:text-xl">Get a New Experiense</p>
                 <h1 class="text-black-800 text-3xl font-bold sm:text-4xl md:text-5xl">
                     SCS CoffeE Lab.
                 </h1>
                 <img src="{{ asset('img/coffee-cup-to-go.png') }}" alt="" class="w-1/4 mb-4">
                 {{-- <div class="flex w-full justify-center my-4"> --}}
-                <div class="grid grid-rows-2 grid-flow-col gap-4">
-
+                <div class="grid">
                     <div class="flex">
                         <div class="w-2/3 bg-yellow-500 hover:bg-yellow-600 transition text-white px-8 py-4 rounded-lg">
                             <a href="{{ route('login') }}" class="">
