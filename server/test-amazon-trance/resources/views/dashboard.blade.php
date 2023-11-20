e4a58edcda854252ba38cb9c250d4479d2b0c89f
sotto97 <sotto.bw97@gmail.com>
2023-11-20 23:59:44 +0900

    レイアウト変更

--- server/test-amazon-trance/resources/views/dashboard.blade.php
+++ server/test-amazon-trance/resources/views/dashboard.blade.php
@@ -1,10 +1,10 @@
 <x-app-layout>
-    <div class="py-10">
+    <div class="py-10 flex justify-center items-center">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <button class=" text-gray-900 hover:text-white bg-white hover:bg-yellow-500 border-yellow-600 border-2 transition overflow-hidden shadow-md sm:rounded-lg">
+            <button class=" text-gray-900 bg-white hover:bg-yellow-200 border-gray-100 hover:border-yellow-200 border-2 transition shadow-md rounded-lg">
                 <div class="block pt-6 pb-4 px-8">
                     <img src="{{ asset('img/coffee-machine.png') }}" alt="" class="w-24 mx-auto text-center">
-                    <p class="p-6">Today's Drip</p>
+                    <p class="p-4">今日のCoffeEを登録</p>
                 </div>
             </button>
         </div>
