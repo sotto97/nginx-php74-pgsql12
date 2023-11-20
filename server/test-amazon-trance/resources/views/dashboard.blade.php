bb5768a23ce07b536d4a4af9d9078d7baa1a70b7
sotto97 <sotto.bw97@gmail.com>
2023-11-20 23:40:25 +0900

    ダッシュボードに追記

--- server/test-amazon-trance/resources/views/dashboard.blade.php
+++ server/test-amazon-trance/resources/views/dashboard.blade.php
@@ -1,11 +1,12 @@
 <x-app-layout>
-    <div class="py-12">
+    <div class="py-10">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+            <button class=" text-gray-900 hover:text-white bg-white hover:bg-yellow-500 border-yellow-600 border-2 transition overflow-hidden shadow-md sm:rounded-lg">
+                <div class="block pt-6 pb-4 px-8">
+                    <img src="{{ asset('img/coffee-machine.png') }}" alt="" class="w-24 mx-auto text-center">
+                    <p class="p-6">Today's Drip</p>
                 </div>
-            </div>
+            </button>
         </div>
     </div>
 </x-app-layout>
